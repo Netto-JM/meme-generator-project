b9e60e8a31af24ee2420775501581c3652ee1916
NettoJM <nettojm86@gmail.com>
2022-09-05 22:01:19 -0300

    implemented upload function

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const memeContainer = document.getElementById('meme-image-container');
 const memeTextContainer = document.getElementById('meme-text');
+const memeImageContainer = document.getElementById('meme-image');
 const memeTextInput = document.getElementById('text-input');
+const memeImageInput = document.getElementById('meme-insert');
 
-function previewText(event) {
+function insertMemeText(event) {
   const memeText = event.target.value;
   memeTextContainer.textContent = memeText;
   console.log(memeText);
 }
 
-memeTextInput.addEventListener('input', previewText);
+function uploadMemeImage(event) {
+  const uploadedImage = event.target.files;
+  const [imageBlob] = uploadedImage;
+  const imageUrl = URL.createObjectURL(imageBlob);
+  memeImageContainer.src = imageUrl;
+}
+
+memeTextInput.addEventListener('input', insertMemeText);
+memeImageInput.addEventListener('change', uploadMemeImage);
