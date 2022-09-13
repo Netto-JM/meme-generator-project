8611fdb6c6909f7688257c7d0a0b8cb30b1c1dfc
NettoJM <nettojm86@gmail.com>
2022-09-13 13:40:31 -0300

    added buttons to change frame style

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const memeContainer = document.getElementById('meme-image-container');
+const memeContainerEl = document.getElementById('meme-image-container');
 const memeTextContainer = document.getElementById('meme-text');
 const memeImageContainer = document.getElementById('meme-image');
 const memeTextInput = document.getElementById('text-input');
 const memeImageInput = document.getElementById('meme-insert');
+const buttonsEl = document.getElementById('buttons');
 
 function insertMemeText(event) {
   const memeText = event.target.value;
@@ -15,7 +16,22 @@ function uploadMemeImage(event) {
   const [imageBlob] = uploadedImage;
   const imageUrl = URL.createObjectURL(imageBlob);
   memeImageContainer.src = imageUrl;
+  /* memeImageContainer.style.border =  */
+}
+
+function changeFrame(event) {
+  const clickedButton = event.target.closest('button');
+  const clickedWhiteSpace = !clickedButton;
+  if (clickedWhiteSpace) return;
+  if (clickedButton.id === 'fire') {
+    memeContainerEl.style.border = '3px dashed red';
+  } else if (clickedButton.id === 'water') {
+    memeContainerEl.style.border = '5px double blue';
+  } else if (clickedButton.id === 'earth') {
+    memeContainerEl.style.border = '6px groove green';
+  }
 }
 
 memeTextInput.addEventListener('input', insertMemeText);
 memeImageInput.addEventListener('change', uploadMemeImage);
+buttonsEl.addEventListener('click', changeFrame);
