81fad563c7bd4e70f934c76de7cbc12b9f434439
NettoJM <nettojm86@gmail.com>
2022-09-13 14:33:08 -0300

    refactored changeFrame to use getFrame as a helper function, implemented useMemePreview function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const memeImageContainer = document.getElementById('meme-image');
 const memeTextInput = document.getElementById('text-input');
 const memeImageInput = document.getElementById('meme-insert');
 const buttonsEl = document.getElementById('buttons');
+const previewImagesEl = document.getElementById('preview-images');
 
 function insertMemeText(event) {
   const memeText = event.target.value;
@@ -19,19 +20,30 @@ function uploadMemeImage(event) {
   /* memeImageContainer.style.border =  */
 }
 
+function getFrame(frameName) {
+  if (frameName === 'fire') return '3px dashed red';
+  if (frameName === 'water') return '5px double blue';
+  if (frameName === 'earth') return '6px groove green';
+}
+
 function changeFrame(event) {
   const clickedButton = event.target.closest('button');
   const clickedWhiteSpace = !clickedButton;
   if (clickedWhiteSpace) return;
-  if (clickedButton.id === 'fire') {
-    memeContainerEl.style.border = '3px dashed red';
-  } else if (clickedButton.id === 'water') {
-    memeContainerEl.style.border = '5px double blue';
-  } else if (clickedButton.id === 'earth') {
-    memeContainerEl.style.border = '6px groove green';
-  }
+  memeContainerEl.style.border = getFrame(clickedButton.id);
+}
+
+function useMemePreview(event) {
+  const clickedImg = event.target.closest('img');
+  const clickedWhiteSpace = !clickedImg;
+  if (clickedWhiteSpace) return;
+  const memePreviewSrc = clickedImg.src.slice(-14);
+  const memePreviousSrc = memeImageContainer.src.slice(-14);
+  if (memePreviewSrc === memePreviousSrc) return;
+  memeImageContainer.src = memePreviewSrc;
 }
 
 memeTextInput.addEventListener('input', insertMemeText);
 memeImageInput.addEventListener('change', uploadMemeImage);
 buttonsEl.addEventListener('click', changeFrame);
+previewImagesEl.addEventListener('click', useMemePreview);
